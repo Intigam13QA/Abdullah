3d9d5bb11dcc4f9a3ca2c5fe2c2646774640d89b
Abdullah13706 <127724839+Abdullah13706@users.noreply.github.com>
2023-09-14 17:31:12 +0300

    Initial commit

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main (String[] args) {
+
+        byte b1 = 25;
+        byte b2 = -25;
+        //byte b3 = 128
+        System.out.println("b1 = " + b1);
+        System.out.println("b2 =" + b2);
+    }
+}
